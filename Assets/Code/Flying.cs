e1540950183f7ed587d6169a145b03fde029dbbe
SzyllerW <szyllerwiktoria@gmail.com>
2025-10-19 00:53:03 +0200

    Update

--- Assets/Code/Flying.cs
+++ Assets/Code/Flying.cs
@@ -27,4 +27,9 @@ public class Flying : MonoBehaviour
     {
         transform.rotation = Quaternion.Euler(0, 0, _rb.velocity.y * _rotationSpeed);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.instance.GameOver();
+    }
 }
